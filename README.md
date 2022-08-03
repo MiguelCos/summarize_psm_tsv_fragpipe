536c42cb9b447d046d34fc565d6c84f1e20b68ad
Miguel Cosenza <migueljcc5@gmail.com>
2022-08-03 11:24:27 +0200

    commit script v0.1 and first README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# summarize_psm_tsv_fragpipe
-Small script to summarize peptides + modifications from the psm.tsv file (Fragpipe output)
+# Summarize `psm.tsv` file from Fragpipe output after TMT search
+
+Small script to summarize peptides + modifications from the psm.tsv file (Fragpipe output) after TMT experiment.
+
+The `peptide.tsv` file from Fragpipe is summarizing different modified versions of the same peptide into a single peptide sequence. This is not optimal when the modified identity of the peptide is important (i.e. when doing a search using N-terminal acetylation as variable modification).
+
+This simple script is currently only working for one-mixture TMT experiments.
+
+## How to use the script? 
+
+1. Download and unzip this repo into a new folder in your PC.
+2. Ideally, initiallize an RStudio project based on the downloaded repo.
+3. Add your `psm.tsv` and `annotation.txt` file in the `data/` folder.
+  Note: the `annotation.txt` file is the one generated and/or required by TMT integrator for summarization.
+4. Open the `summarize_psm_tsv_fragpipe.R` script.
+5. Click on `Source`, on the top right corned.
+
+## What does it do? 
+
+It will:
+
+1. Load the `psm.tsv` file.
+2. Load the `annotation.txt` file
+3. Map the sample names of the annotation file to the column names of the TMT reporter ion intensities in the PSM file.
+4. Sum the total reporter ion intensities.
+5. Filter the PSM file to keep only the best PSM per modified peptide under the following criteria:
+  - Only PSMs with PeptideProbability > 0.9.
+  - Only PSMs with summed reporter ion intensities > 0.
+  - If one modified peptide has several redundant PSMs, select the PSM with best Purity.
+  - If one modified peptide has severak reduntant PSMs with the same purity, then select the PSM with the highest summed reporter ion intensity.
+6. Generate a new filtered `psm.tsv` file, located in the `output/` folder and called `best_modified_peptides_psm.tsv`.
+
+
---
+++ summarize_psm_tsv_fragpipe.R
@@ -0,0 +1,91 @@
+# script to summarize the psm.tsv table from Fragpipe to keep peptide + modification
+# Miguel Cosenza
+# v 0.1
+
+# required packages ----
+
+library(tidyverse)
+library(here)
+
+# required data ----------------------------------------
+
+# psm file
+psms <- read_tsv(here("data/psm.tsv"))
+
+# load data annotation
+annot <- read_delim(here("data/annotation.txt"),
+                    col_names = FALSE) %>%
+  # set column names to channel and sample respectively
+  # this is necessary to select the TMT channel columns from the psm.tsv
+  dplyr::rename(channel = X1, sample = X2)
+
+
+# pre-process annotation file --------------------------
+
+# we need to modify the sample names in the annotation file to match the 
+# names of the of the TMT intensities after loading into R.
+
+psm_cols_trim <- colnames(psms) %>% # get column names of psm file
+  str_remove("\\..*") # eliminate sufix 
+
+psm_cols <- colnames(psms) # get column names of psm file wo eliminate sufix
+
+# create df mapping trimmed sample names and the ones with sufix
+trimed2original_sample <- tibble(sample = psm_cols_trim,
+                                 sample_trim = psm_cols)
+
+# correct annotation file to match column names for TMT channel intensities
+# in the psm file
+annot_2 <- annot %>%
+  left_join(., trimed2original_sample) %>% # merge annotation file w untrimmed
+  slice(1:nrow(annot)) # keep only sample names present in columns of psm file
+
+
+# processing and filtering psm.tsv file -----------------------------------
+
+## create reduced dataframe ----
+psm_mod1 <- psms %>% 
+  # select interesting columns for filtering and testing
+  dplyr::select(`Peptide`, `Modified Peptide`,Spectrum, Purity, 
+                Intensity,`PeptideProphet Probability`, `Is Unique`, 
+                `Quan Usage`, annot_2$sample_trim) %>%
+  rowwise() %>% 
+  # generate column  of summed intensities of TMT channels
+    # the selection of the cols representing the TMT channels is based on 
+    # the annot_2 object 
+  mutate(summ_int = sum(c_across(cols = annot_2$sample_trim))) 
+
+
+## filter the reduced dataframe ----
+
+psm_mod2 <- psm_mod1 %>% 
+  # keep only psms with summed TMT intensity > 0
+  filter(summ_int > 0) %>% 
+  # keep only psms with peptideProphet prob > 0.9
+  filter(`PeptideProphet Probability` > 0.9) %>%
+  # group by modified peptide
+  group_by(`Modified Peptide`) %>% 
+  # if several PSMs to the same modified peptide, select one with best Purity
+  slice_max(n = 1, 
+            order_by = c(Purity)) %>% 
+  # if a modified peptide was identified with several PSMs and same high purity,
+  # then select the PSMs with highest summed TMT intensity
+  slice_max(n = 1,
+            order_by = summ_int) 
+
+## filter the original psm.tsv file ----
+
+best_modified_psms <- psms %>%
+  # keep psms after filtering by purity and summed intensity
+  filter(Spectrum %in% psm_mod2$Spectrum)
+
+## save the summarized psm.tsv file ----
+
+write_tsv(x = best_modified_psms,
+          file = here("output/best_modified_peptides_psm.tsv"))
+
+
+
+
+  
+  
